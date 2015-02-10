dd632c83907d4f5b926e6d6ba47842fe027b17bb
Julia Rechkunova <julia.rechkunova@gmail.com>
2015-02-10 15:01:00 +0600

    Update tasks loader

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 'use strict';
 
 var gulp = require('gulp'),
-	taskLoader = require('gulp-task-loader');
+	tasksLoader = require('gulp-task-loader');
 
 /**
- * Csstime tasks loader
+ * Csstime
  * @constructor
  */
-function CssTimeTasksLoader () {
+function CssTime () {
 
 }
 
 /**
- * Loads tasks
+ * Loads gulp tasks
  */
-CssTimeTasksLoader.prototype.load = function () {
-	taskLoader('./tasks');
+CssTime.prototype.loadGulpTasks = function () {
+	tasksLoader();
 };
 
-module.export = new CssTimeTasksLoader();
+module.exports = new CssTime();
