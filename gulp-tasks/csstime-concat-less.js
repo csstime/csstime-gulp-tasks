0fd3ee8bbcd7ff97e62e6a564bb75992395671d9
Julia Rechkunova <julia.rechkunova@gmail.com>
2015-02-10 17:12:37 +0600

    Add concat-less task

---
+++ gulp-tasks/csstime-concat-less.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+	path = require('path'),
+	util = require('util'),
+	concat = require('gulp-concat'),
+	csstime = require('../index.js'),
+	config = require('../config.json');
+
+var importFormat = '@import "%s/%s/%s/%s/%s"';
+
+module.exports = function () {
+	var components = csstime.getPublishedComponents(),
+		imports = [];
+
+	components.forEach(function (component) {
+		imports.push(util.format(
+			importFormat,
+			config.destinationDir,
+			config.componentsAssetsDir,
+			component,
+			config.lessDir,
+			config.lessIndexFile
+		));
+	});
+
+	return file(config.lessIndexFile, imports.join('\n'), {src: true})
+		.pipe(gulp.dest(config.temporaryDir));
+};
--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 var gulp = require('gulp'),
 	path = require('path'),
+	fs = require('fs'),
+	config = require('./config.json'),
 	packageConfig = require('./package.json'),
 	tasksLoader = require('gulp-task-loader');
 
@@ -26,6 +28,35 @@ CssTime.prototype.loadGulpTasks = function () {
 			packageConfig.name,
 			GULP_TASKS_DIR
 		));
+};
+
+/**
+ * Gets components names
+ */
+CssTime.prototype.getPublishedComponents = function () {
+	var publishedComponentsDir = path.join(
+				process.cwd(),
+				config.destinationDir,
+				config.componentsAssetsDir
+			);
+
+	if (!fs.existsSync(publishedComponentsDir)) {
+		return [];
+	}
+
+	var	directories = fs.readdirSync(publishedComponentsDir)
+			.filter(function (file) {
+				return fs.statSync(path.join(publishedComponentsDir, file)).isDirectory();
+			});
+
+	var mainIndex = directories.indexOf(config.indexComponentName);
+	if (mainIndex === -1) {
+		return directories;
+	}
+	directories.splice(mainIndex, 1);
+	directories.unshift(config.indexComponentName);
+
+	return directories;
 };
 
 module.exports = new CssTime();
