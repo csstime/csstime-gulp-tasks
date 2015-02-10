4690daffa031cf5a7e1c26e8e578a90f1c13bd07
Julia Rechkunova <julia.rechkunova@gmail.com>
2015-02-10 17:38:57 +0600

    Update concat-less task

--- gulp-tasks/csstime-concat-less.js
+++ gulp-tasks/csstime-concat-less.js
@@ -1,28 +1,34 @@
 var gulp = require('gulp'),
 	path = require('path'),
 	util = require('util'),
+	fs = require('fs'),
 	file = require('gulp-file'),
 	concat = require('gulp-concat'),
 	csstime = require('../index.js'),
 	config = require('../config.json');
 
-var importFormat = '@import "%s/%s/%s/%s/%s"';
+var IMPORT_FORMAT = '@import "%s"',
+	FILE_FORMAT = "%s/%s/%s/%s/%s";
 
 module.exports = function () {
 	var components = csstime.getPublishedComponents(),
 		imports = [];
 
 	components.forEach(function (component) {
-		imports.push(util.format(
-			importFormat,
+		var importingFile = util.format(
+			FILE_FORMAT,
 			config.destinationDir,
 			config.componentsAssetsDir,
 			component,
 			config.lessDir,
 			config.lessIndexFile
-		));
+		);
+		if (!fs.existsSync(path.join(process.cwd(), importingFile))) {
+			return;
+		}
+		imports.push(util.format(IMPORT_FORMAT, importingFile));
 	});
 
 	return file(config.lessIndexFile, imports.join('\n'), {src: true})
-		.pipe(gulp.dest(config.temporaryDir));
+		.pipe(gulp.dest(path.join(config.destinationDir, config.temporaryDir)));
 };
