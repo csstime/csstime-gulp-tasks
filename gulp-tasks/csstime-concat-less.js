9ffe01ea80633e38936f7335c74e05e825a2a427
Julia Rechkunova <julia.rechkunova@gmail.com>
2015-02-11 17:56:53 +0600

    Add sprites

--- gulp-tasks/csstime-concat-less.js
+++ gulp-tasks/csstime-concat-less.js
@@ -8,16 +8,36 @@ var gulp = require('gulp'),
 	csstime = require('../index.js'),
 	config = require('../config.json');
 
-var IMPORT_FORMAT = '/*\n * Styles of component "%s"\n */\n@import "%s";',
-	FILE_FORMAT = '%s/%s/%s/%s/%s';
+var IMPORT_SPRITES_FORMAT = '/*\n * Sprites\n */\n@import "%s";',
+	IMPORT_FORMAT = '/*\n * Styles of component "%s"\n */\n@import "%s";',
+	BASE_VARIABLES = '@CDN: "%s";',
+	SPRITES_VARIABLES = '@SPRITES_IMAGE: "%s";';
 
 module.exports = function () {
 	var components = csstime.getPublishedComponents(),
 		imports = [];
 
+	// variables
+	imports.push(util.format(BASE_VARIABLES, config.cdnPath));
+
+	// sprites
+	if (config.useImageSprites) {
+		imports.push(util.format(
+			SPRITES_VARIABLES,
+			config.spritesFileName + '.png'
+		));
+
+		var importingSpriteFile = path.join(
+			config.destinationDir,
+			config.temporaryDir,
+			config.spritesFileName + '.less'
+		);
+		imports.push(util.format(IMPORT_SPRITES_FORMAT, importingSpriteFile));
+	}
+
+	// less
 	components.forEach(function (component) {
-		var importingFile = util.format(
-			FILE_FORMAT,
+		var importingFile = path.join(
 			config.destinationDir,
 			config.componentsAssetsDir,
 			component,
@@ -30,6 +50,7 @@ module.exports = function () {
 		imports.push(util.format(IMPORT_FORMAT, component, importingFile));
 	});
 
+	// create file
 	return file(config.stylesFileName + '.less', imports.join('\n\n'), {src: true})
 		.pipe(gulp.dest(path.join(config.destinationDir, config.temporaryDir)));
 };
