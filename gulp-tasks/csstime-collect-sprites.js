f5199b4425a8bcacddc013044f077736e9041d2c
Julia Rechkunova <julia.rechkunova@gmail.com>
2015-02-11 18:19:03 +0600

    Add collect-sprites task

---
+++ gulp-tasks/csstime-collect-sprites.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var gulp = require('gulp'),
+	path = require('path'),
+	spritesmith = require('gulp.spritesmith'),
+	spritesmithConfig = require('../.spritesmith.json'),
+	config = require('../config.json');
+
+module.exports = function () {
+	var spriteData = gulp.src(path.join(
+			config.destinationDir,
+			config.componentsAssetsDir,
+			'*',
+			config.spritesDir,
+			'*',
+			'*.png'
+		))
+		.pipe(spritesmith({
+			imgName: config.spritesFileName + '.png',
+			cssName: config.spritesFileName + '.less',
+			algorithm: spritesmithConfig.algorithm,
+			cssTemplate: spritesmithConfig.cssTemplate,
+			padding: spritesmithConfig.padding
+		}));
+
+	spriteData.img.pipe(gulp.dest(config.destinationDir));
+
+	return spriteData.css.pipe(gulp.dest(path.join(
+		config.destinationDir,
+		config.temporaryDir,
+		config.spritesFileName + '.less'
+	)));
+};
