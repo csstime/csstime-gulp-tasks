d74815e4766909cad1c81ade231f8f40174b6389
Julia Rechkunova <julia.rechkunova@gmail.com>
2015-02-12 12:18:04 +0600

    Add collect-images task

---
+++ gulp-tasks/csstime-collect-images.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var gulp = require('gulp'),
+	path = require('path'),
+	config = require('../config.json');
+
+module.exports = function () {
+	return gulp.src(path.join(
+			config.destinationDir,
+			config.componentsAssetsDir,
+			'*',
+			config.imagesDir
+		))
+		.pipe(gulp.dest(path.join(
+			config.destinationDir,
+			config.staticDir
+		)));
+};
