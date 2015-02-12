61f73ffa884aa41e7fa6c66dd11bffd2cdf6aeea
Julia Rechkunova <julia.rechkunova@gmail.com>
2015-02-12 18:27:57 +0600

    Add configuration

--- index.js
+++ index.js
@@ -2,18 +2,35 @@
 
 var gulp = require('gulp'),
 	path = require('path'),
+	fs = require('fs'),
+	currentConfig = require('./default.config.json'),
 	packageConfig = require('./package.json'),
 	tasksLoader = require('gulp-task-loader');
 
 var NODE_MODULES_DIR = 'node_modules',
-	GULP_TASKS_DIR = 'gulp-tasks';
+	GULP_TASKS_DIR = 'gulp-tasks',
+	CONFIG_FILE_NAME = 'config.json';
 
 module.exports = {
 
 	/**
 	 * Loads gulp tasks
 	 */
-	loadGulpTasks: function () {
+	loadGulpTasks: function (options) {
+		if (options) {
+			options.keys().forEach(function (key) {
+				if (key in currentConfig) {
+					return;
+				}
+				currentConfig[key] = options[key];
+			});
+		}
+
+		fs.writeSync(
+			path.join('.', CONFIG_FILE_NAME),
+			JSON.stringify(currentConfig)
+		);
+
 		tasksLoader(
 			path.join(
 				NODE_MODULES_DIR,
