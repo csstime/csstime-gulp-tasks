d8aa56d01c1951cca0a285194c397ef441b2aedd
Oleg Kalyanov <o.kalyanov@flamp.ru>
2016-02-15 14:45:25 +0600

    Handle excludes paths in csscomb task

--- gulp-tasks/csstime-exec-csscomb.js
+++ gulp-tasks/csstime-exec-csscomb.js
@@ -11,12 +11,16 @@ module.exports = function (gulp, plugins, config) {
 					defaultCsscombConfig.configPath) ?
 					path.join(config.packagePath,
 						defaultCsscombConfig.configPath) :
-					config.csscombConfig.configPath;
-
-			return gulp.src([
+					config.csscombConfig.configPath,
+				excludes = config.csscombConfig.excludes.map(function(path) {
+					return '!' + path;
+				}),
+				sources = [
 					path.join(config.csscombConfig.sources, '**', '*.less'),
 					path.join(config.csscombConfig.sources, '**', '*.css')
-				])
+				];
+
+			return gulp.src(sources.concat(excludes))
 				.pipe(plugins.csscomb(configPath))
 				.pipe(gulp.dest(config.csscombConfig.sources));
 		}
